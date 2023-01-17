617980bec528ea5a5a06c7986aedf3ffc6d544af
Oleg Iraklionov <irakliov@mail.ru>
2023-01-17 19:49:30 +0300

    Case 25 is ready

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+//3, 5 -> 243 (3⁵)
+
+//2, 4 -> 16
+int getUserDate(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int UserData = int.Parse(Console.ReadLine()!);
+    return UserData;
+}
+int NumberOfNumber(int Osnova, int Step)
+{
+    int result = 1;
+    for (int i = 1; i <= Step; i++)
+    {
+        result = Osnova * result;
+    }
+    return result;
+}
+
+int Osn = getUserDate("Введите значения основания");
+int St = getUserDate("Введите значения степени");
+int Stepen = NumberOfNumber(Osn, St);
+Console.WriteLine(Stepen);
